7fc489ca610c6873bfc3c5d8702e31d2a0f3db09
undead25 <651182414@qq.com>
2017-11-22 23:21:55 +0800

    organize questions

--- src/generate.js
+++ src/generate.js
@@ -1,10 +1,9 @@
-const Metalsmith = require('metalsmith');
-const render = require('consolidate').handlebars.render;
 const ora = require('ora');
-const match = require('minimatch')
+const Metalsmith = require('metalsmith');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
 const { execSync } = require('child_process');
-
-const prompt = require('./prompt');
+const render = require('consolidate').handlebars.render;
 
 /**
  * Template plugin for metalsmith
@@ -31,21 +30,18 @@ const renderTemplate = (files, metalsmith, done) => {
 
 const filterFiles = (files, metalsmith, done) => {
   const { filters } = metalsmith.metadata();
+  !filters && done();
+
   const fileNames = Object.keys(files);
-  Object.keys(filters).forEach(glob => {
+  Object.keys(filters).forEach(filterFile => {
     fileNames.forEach(file => {
-      if (match(file, glob, { dot: true })) {
-        const condition = filters[glob];
-        if (!metalsmith.metadata()[condition]) {
-          console.log(file)
-        }
-        // console.log(condition)
-        // if (!evaluate(condition, metalsmith.metadata())) {
-        //   delete files[file]
-        // }
+      if (file === filterFile) {
+        const condition = filters[filterFile];
+        !metalsmith.metadata()[condition] && delete files[file];
       }
     })
-  })
+  });
+  done();
 }
 
 const getUser = () => execSync('git config --get user.name').toString().trim();
@@ -53,11 +49,79 @@ const getUser = () => execSync('git config --get user.name').toString().trim();
 const getConfig = (files, metalsmith, done) => {
   Object.keys(files).forEach(file => {
     if (file === 'meta.json') {
-      const { filters } = JSON.parse(files[file].contents.toString());
-      metalsmith.metadata({ filters });
+      const { filters, prompts } = JSON.parse(files[file].contents.toString());
+      metalsmith.metadata({ filters, prompts });
       done();
     }
   })
+};
+
+const init = async (files, metalsmith, done) => {
+  console.log('\n  ' + chalk.bgGreen(chalk.black(' Welcome to rsg hack! \n')));
+  const { target } = await inquirer.prompt({
+    type: 'list',
+    name: 'target',
+    message: 'Which type of app you want to develop?',
+    choices: [
+      'Web Application'
+      // 'Library',
+      // 'Node RESTful',
+      // 'UI Components'
+    ]
+  })
+
+  if (target === 'Web Application') {
+    const { framework } = await inquirer.prompt({
+      type: 'list',
+      name: 'framework',
+      message: 'Please choose a framework',
+      choices: [
+        'React',
+        'Angular',
+        'Vue'
+      ]
+    });
+
+    if (framework === 'React') {
+      // @todo clone appropriate repo
+      done();
+      // const { typescript } = await inquirer.prompt({
+      //   type: 'confirm',
+      //   name: 'typescript',
+      //   message: 'Using TypeScript?'
+      // });
+
+      // const { sass } = await inquirer.prompt({
+      //   type: 'confirm',
+      //   name: 'sass',
+      //   message: 'Using Sass for style?'
+      // });
+
+      // const { less } = await inquirer.prompt({
+      //   type: 'confirm',
+      //   name: 'less',
+      //   message: 'Using Less for style?'
+      // });
+
+      // await metalsmith.metadata({ ...metalsmith.metadata(), typescript, sass, less });
+      // done();
+    } else {
+      ora().info('Woking in progress & thanks for your intention!');
+      done();
+    }
+  };
+};
+
+const questions = (files, metalsmith, done) => {
+  const { prompts } = metalsmith.metadata();
+  console.log(prompts)
+  !prompts && done();
+  Object.keys(prompts).forEach(prompt => {
+    const { type, message } = prompts[prompt];
+    inquirer.prompt({
+      type, message, name: prompt
+    });
+  });
 }
 
 const generate = () => {
@@ -69,11 +133,12 @@ const generate = () => {
 
   metalsmith.source('./download')
     .metadata(metadata)
+    .use(init)
     .use(getConfig)
-    .use(prompt)
+    .use(questions)
     .use(filterFiles)
     .use(renderTemplate)
-    .build(err => err && ora().fail(err))
+    .build(err => err && ora().fail(err));
 }
 
 generate()
