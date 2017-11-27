76a97b1a0755033fc0e0ff7e4dcbf85b9847f5c2
undead25 <651182414@qq.com>
2017-11-28 00:20:26 +0800

    fullfil the basic usage

--- bin/rsg.js
+++ bin/rsg.js
@@ -14,15 +14,19 @@ commander
   .option('-v', 'version', () => ora().info(pkg.version))
   .parse(process.argv);
 
-if (!project) {
+const errorLog = err => {
   console.log();
-  ora().fail(chalk.redBright('Please specify the project name:'));
-  console.log(`  ${chalk.blue('rsg')} ${chalk.green('<project-name>')} \n`);
+  ora().fail(chalk.redBright(err));
+  console.log(`  Usage: ${chalk.blue('rsg')} ${chalk.green('<project-name>')} \n`);
   ora().info('For example:');
   console.log(`  ${chalk.blue('rsg')} ${chalk.green('my-project')} \n`);
   ora().info('For help:');
   console.log(`  ${chalk.blue('rsg')} ${chalk.green('--help')} \n`);
   process.exit(1);
 }
+
+!project && errorLog('Please specify the project name.')
+
+process.argv.length > 3 && errorLog('Too many arguments.')
 
 rsg(project);
